49544fe03caef3f7c1aa273e94ac1a76a182427c
Matt McCormick <matt.mccormick@kitware.com>
2024-07-24 16:16:58 -0400

    WIP: ENH: Pixi ITK-Wasm config

---
+++ src/wasm/itk_wasm_env.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+function die() {
+    echo "$1"
+    exit 1
+}
+
+export ITK_WASM_DESCRIPTION=${ITK_WASM_DESCRIPTION:-$(cat package.json | jq -e -r '."itk-wasm"."description"')}
+if [[ "${ITK_WASM_DESCRIPTION}" = "null" ]]; then
+    die "itk-wasm.description not set in package.json"
+fi
+
+export ITK_WASM_REPOSITORY=${ITK_WASM_REPOSITORY:-$(cat package.json | jq -e -r '."itk-wasm"."repository"')}
+if [[ "${ITK_WASM_REPOSITORY}" = "null" ]]; then
+    die "itk-wasm.repository not set in package.json"
+fi
+
+export ITK_WASM_EMSCRIPTEN_DOCKER_IMAGE=${ITK_WASM_EMSCRIPTEN_DOCKER_IMAGE:-$(cat package.json | jq -e -r '."itk-wasm"."emscripten-docker-image" // "itkwasm/emscripten:latest"')}
+export ITK_WASM_WASI_DOCKER_IMAGE=${ITK_WASM_WASI_DOCKER_IMAGE:-$(cat package.json | jq -e -r '."itk-wasm"."wasi-docker-image" // "itkwasm/wasi:latest"')}
+
+export ITK_WASM_TYPESCRIPT_PACKAGE_NAME=${ITK_WASM_TYPESCRIPT_PACKAGE_NAME:-$(cat package.json | jq -e -r '."itk-wasm"."typescript-package-name"')}
+if [[ "${ITK_WASM_TYPESCRIPT_PACKAGE_NAME}" = "null" ]]; then
+    die "itk-wasm.typescript-package-name not set in package.json"
+fi
+export ITK_WASM_TYPESCRIPT_OUTPUT_DIR=${ITK_WASM_TYPESCRIPT_OUTPUT_DIR:-$(cat package.json | jq -e -r '."itk-wasm"."typescript-output-dir" // "typescript"')}
+
+export ITK_WASM_PYTHON_PACKAGE_NAME=${ITK_WASM_PYTHON_PACKAGE_NAME:-$(cat package.json | jq -e -r '."itk-wasm"."python-package-name"')}
+if [[ "${ITK_WASM_PYTHON_PACKAGE_NAME}" = "null" ]]; then
+    die "itk-wasm.python-package-name not set in package.json"
+fi
+export ITK_WASM_PYTHON_OUTPUT_DIR=${ITK_WASM_PYTHON_OUTPUT_DIR:-$(cat package.json | jq -e -r '."itk-wasm"."python-output-dir" // "python"')}
+
+env
